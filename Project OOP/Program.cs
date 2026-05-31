fcd4b64eebfcbf711aa18d378ef2fb018af34864
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 20:15:37 +0300

    Update Program.cs

--- Project OOP/Program.cs
+++ Project OOP/Program.cs
@@ -7,46 +7,42 @@ namespace TaxiDispatcher
     {
         static void Main(string[] args)
         {
-            // Вмикаємо UTF-8 для коректного відображення української мови!
             Console.OutputEncoding = Encoding.UTF8;
 
-            Console.WriteLine("ПІБ студента: Зварич Владислав Вячеславович");
-            Console.WriteLine("Курс: 1 | Група: ІПЗ-12 | Варіант: 44");
-            Console.WriteLine("Версія 5 (Успадкування, Абстрактні класи, Інтерфейси)\n");
+            Logger.Log("ПІБ студента: Зварич Владислав В'ячеславович");
+            Logger.Log("Навчальний заклад: КНУ ім. Тараса Шевченка | Група: ІПЗ-12");
+            Logger.Log("Варіант: 44 | Версія 5 (Абстракції + Вивід у JSON)\n");
 
-            // Тест абстрактного класу Person (Створення нащадків)
             Passenger p1 = new Passenger("Олексій", "+380931112233", 500.0);
             Driver d1 = new Driver("Микола", "+380501234567", "Renault", "AA1111BB");
             Driver d2 = new Driver("Степан", "+380671234567", "Skoda", "BC2222CC");
 
-            Console.WriteLine("--- ПЕРЕВІРКА ПОЛІМОРФІЗМУ (Абстрактний метод PrintRole) ---");
-            // Можемо покласти їх в масив базового типу Person
+            Logger.Log("--- ПЕРЕВІРКА ПОЛІМОРФІЗМУ ---");
             Person[] users = { p1, d1, d2 };
             foreach (var user in users)
             {
                 user.PrintRole();
             }
 
-            Console.WriteLine("\n--- ЗАДАЧА 3 ПРІОРИТЕТУ: Автоматична черга на стоянці ---");
+            Logger.Log("\n--- АВТОМАТИЧНА ЧЕРГА НА СТОЯНЦІ ---");
             DispatcherSystem dispatcher = new DispatcherSystem("SmartCity Taxi");
 
-            // Водії приїжджають на базу
             dispatcher.AddDriverToStand(d1);
             dispatcher.AddDriverToStand(d2);
 
-            // Клієнт замовляє таксі, беремо першого з черги
             Driver assignedDriver = dispatcher.GetNextAvailableDriver();
 
             if (assignedDriver != null)
             {
-                // Використовуємо інтерфейс ITripManager
                 ITripManager order1 = new Order(1, p1, assignedDriver, "Вокзал", "Хрещатик");
 
                 dispatcher.DispatchLog((Order)order1);
                 order1.CompleteTrip();
             }
 
-            Console.WriteLine("\n[Система]: Роботу завершено. Натисніть Enter для виходу.");
+            // ФІНАЛЬНИЙ АКОРД: Зберігаємо всю історію в JSON
+            Logger.SaveToJson("simulation_result.json");
+
             Console.ReadLine();
         }
     }
