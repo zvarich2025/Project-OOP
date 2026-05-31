e7be9881ceeadafb53d74985ad4c9af1dbe84f86
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 21:53:21 +0300

    Update Driver.cs

--- Project OOP/Driver.cs
+++ Project OOP/Driver.cs
@@ -6,22 +6,27 @@ namespace TaxiDispatcher
     {
         public string CarModel { get; set; }
         public string LicensePlate { get; }
-        public bool IsAvailable { get; set; }
+        public TaxiClass CarClass { get; set; }
+        public DriverState State { get; set; } // Стан (обід, з клієнтом тощо)
+        public DateTime ShiftStartTime { get; private set; } // Початок зміни
         public int CompletedTrips { get; set; }
 
-        public Driver(string name, string phoneNumber, string carModel, string licensePlate) : base(name, phoneNumber)
+        public Driver(string name, string phoneNumber, string carModel, string licensePlate, TaxiClass carClass)
+            : base(name, phoneNumber)
         {
             CarModel = carModel;
             LicensePlate = licensePlate;
-            IsAvailable = true;
+            CarClass = carClass;
+            State = DriverState.OnStand;
+            ShiftStartTime = DateTime.Now.AddHours(-2); // Ніби водій працює вже 2 години
             CompletedTrips = 0;
         }
 
-        public override void PrintRole() => Logger.Log($"[Роль]: Водій {Name} на {CarModel}");
+        public TimeSpan GetShiftDuration() => DateTime.Now - ShiftStartTime;
 
-        public bool IsReadyForOrder() => IsAvailable;
+        public override void PrintRole() => Logger.Log($"[Роль]: Водій {Name} на {CarModel} (Клас: {CarClass})");
+
+        public bool IsReadyForOrder() => State == DriverState.OnStand;
         public static Driver operator ++(Driver d) { d.CompletedTrips++; return d; }
-        public static Driver operator -(Driver d) { d.IsAvailable = false; return d; }
-        public static Driver operator +(Driver d) { d.IsAvailable = true; return d; }
     }
 }
