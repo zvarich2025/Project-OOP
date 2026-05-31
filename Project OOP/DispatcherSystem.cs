913c502ea1d2ef6be7b843e40e8658981a3db87c
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 19:39:19 +0300

    feat: implement secondary priority tasks in Dispatcher
    Added MakeVip method. Implemented discount logic using overloaded division operator.

--- Project OOP/DispatcherSystem.cs
+++ Project OOP/DispatcherSystem.cs
@@ -5,30 +5,30 @@ namespace TaxiDispatcher
     public class DispatcherSystem
     {
         public string Title { get; set; }
-        public bool IsActive { get; set; }
 
-        public DispatcherSystem() : this("Єдина Диспетчерська") { }
+        public DispatcherSystem(string title) { Title = title; }
 
-        public DispatcherSystem(string title)
+        // ЗАДАЧА 2 ПРІОРИТЕТУ: Робота з VIP клієнтами
+        public void MakeVip(Passenger passenger)
         {
-            Title = title;
-            IsActive = true;
+            passenger.IsVip = true;
+            Console.WriteLine($"[Диспетчерська]: Пасажир {passenger.Name} отримав статус VIP!");
         }
 
-        // Метод координації бізнес-процесу
         public void DispatchLog(Order order)
         {
             Console.WriteLine($"[Диспетчер {Title}]: Обробка замовлення №{order.OrderId}...");
-            bool success = order.ProcessOrder();
 
-            if (success)
+            // Якщо клієнт VIP - робимо знижку 20% (використовуємо перевантажений оператор /)
+            if (order.CurrentPassenger.IsVip)
             {
-                Console.WriteLine($"[УСПІХ]: Замовлення схвалено! Вартість: {order.Price} грн. Статус: {order.Status}");
+                order = order / 1.2;
+                Console.WriteLine($"Застосовано VIP знижку! Нова ціна: {Math.Round(order.Price, 2)} грн.");
             }
-            else
-            {
-                Console.WriteLine($"[ВІДХИЛЕНО]: Не вдалося виконати. Причина: {order.Status}");
-            }
+
+            bool success = order.ProcessOrder();
+            if (success) Console.WriteLine($"[УСПІХ]: Замовлення схвалено! Статус: {order.Status}");
+            else Console.WriteLine($"[ВІДХИЛЕНО]: Не вдалося виконати.");
         }
     }
 }
