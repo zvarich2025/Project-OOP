f5693c212d4c3d9477ad4068f6fccba83e4ff431
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 20:15:48 +0300

    Update Driver.cs

--- Project OOP/Driver.cs
+++ Project OOP/Driver.cs
@@ -9,7 +9,6 @@ namespace TaxiDispatcher
         public bool IsAvailable { get; set; }
         public int CompletedTrips { get; set; }
 
-        // ВИПРАВЛЕНО: Правильний виклик базового конструктора
         public Driver(string name, string phoneNumber, string carModel, string licensePlate) : base(name, phoneNumber)
         {
             CarModel = carModel;
@@ -18,7 +17,8 @@ namespace TaxiDispatcher
             CompletedTrips = 0;
         }
 
-        public override void PrintRole() => Console.WriteLine($"[Роль]: Водій {Name} на {CarModel}");
+        // Використовуємо Logger
+        public override void PrintRole() => Logger.Log($"[Роль]: Водій {Name} на {CarModel}");
 
         public bool IsReadyForOrder() => IsAvailable;
         public static Driver operator ++(Driver d) { d.CompletedTrips++; return d; }
