51f0c8df584e6ba95c349ba0dfdfd98ea8fe4d7b
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 21:53:12 +0300

    Create DispatcherEmployee.cs

---
+++ Project OOP/DispatcherEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaxiDispatcher
+{
+    // Диспетчер - це також Person (Успадкування)
+    public class DispatcherEmployee : Person
+    {
+        public DispatcherRole Role { get; set; }
+        public int HandledOrders { get; set; }
+
+        public DispatcherEmployee(string name, string phone, DispatcherRole role) : base(name, phone)
+        {
+            Role = role;
+            HandledOrders = 0;
+        }
+
+        public override void PrintRole() => Logger.Log($"[Бригада]: Диспетчер {Name}, Роль: {Role}");
+    }
+}
