3fccdf76408f627f266a93f4e1ceee8ec64a78a4
zvarich2025 <zvarich2025@gmail.com>
2026-05-31 21:54:04 +0300

    Create TaxiExceptions.cs

---
+++ Project OOP/TaxiExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaxiDispatcher
+{
+    public class InsufficientFundsException : Exception
+    {
+        public double RequiredAmount { get; }
+        public InsufficientFundsException(string message, double requiredAmount) : base(message)
+        {
+            RequiredAmount = requiredAmount;
+        }
+    }
+
+    public class DriverUnavailableException : Exception
+    {
+        public DriverUnavailableException(string message) : base(message) { }
+    }
+
+    public class InvalidRouteException : Exception
+    {
+        public InvalidRouteException(string message) : base(message) { }
+    }
+}
